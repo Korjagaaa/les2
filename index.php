c18e06b57d0237dde12c266a8653cdbed8c6f36c
Korjagaaa <36592057+Korjagaaa@users.noreply.github.com>
2018-02-21 16:34:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>		
+
+
+<form action="">
+Введите число:<input type="number" name="x">
+<input type="submit">
+</form>
 <?php 
    $a = $_GET['x'];
    $b = 1; // переменная 1
@@ -23,3 +36,5 @@
    		}
     }
 ?>
+</body>
+</html>
